08893846c8b8ccfc0513634ab45a656789b531e9
Finn W. Kinnear <finn.kinnear@gmail.com>
2016-03-29 23:42:46 +1300

    Add basic search functionality

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,6 +1,27 @@
 class FilesController < ApplicationController
+	before_action :open_session
+
+	DB_NAME = "Colenso"
+	XQUERY = "XQUERY "
+	NAMESPACE = "declare default element namespace 'http://www.tei-c.org/ns/1.0'; "
+	ITERATE_WHOLE_COLLECTION = "for $n in (collection('#{DB_NAME}')) "
+	RETURN_PATH =  " return db:path($n)"
+
 	def index
-		@session = BaseXClient.session
-		@results = @session.execute('LIST Colenso').split("\n")
+		@results = @session.execute("LIST #{DB_NAME}").split("\r")
+	end
+
+	def search
+		search_input = params[:search]
+		query = XQUERY + NAMESPACE + ITERATE_WHOLE_COLLECTION + "where $n contains text '#{search_input}'" + RETURN_PATH
+		query_result = @session.execute(query)
+		@search_result = query_result.split("\n")
+	end
+
+	private
+
+	def open_session
+		@session ||= BaseXClient.session
+		@session.execute("OPEN #{DB_NAME}")
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'files#index'
   get '/files' => 'files#index'
+  get '/search' => 'files#search'
 end
